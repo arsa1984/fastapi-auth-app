3be87bc7b86dafd28ffb9264c259ac71fc29ce77
arsa1984 <arsakalhor@gmail.com>
2025-09-26 10:04:06 +0330

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,11 @@
-# backend/app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://fastapi_user:fastapi_pass@db:5432/fastapi_auth"
-
+DATABASE_URL = os.environ["DATABASE_URL"]  
 
 engine = create_engine(DATABASE_URL)
-
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 Base = declarative_base()
